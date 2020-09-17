9973d22d57051665d2991ffb16e6a33be7833fa6
gokmensalih34 <gokmensalih34@gmail.comgit config --listgit config --listgit config --global user.password 34253812SGsg>
2020-09-17 19:53:12 +0200

    add youtube

---
+++ src/test/java/com/youtube/runners/CukesRunner.java
@@ -0,0 +1,22 @@
+package com.youtube.runners;
+
+import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
+import org.junit.runner.RunWith;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(
+        plugin = {"json:target/cucumber.json",
+        "html:target/default-html-reports"},
+        features = "src/test/resources/features",
+        glue = "com/youtube/stepdefinitions",
+        dryRun = true,
+        tags = "@wip"
+)
+
+
+public class CukesRunner {
+
+
+
+}
---
+++ src/test/java/com/youtube/utilities/Driver.java
@@ -0,0 +1,4 @@
+package com.youtube.utilities;
+
+public class Driver {
+}
